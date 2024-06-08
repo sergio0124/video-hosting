87e790ec162114dc4036daee929b1a69c2bcce75
Reznikov Sergey <s.reznikov@promo-z.ru>
2024-06-08 19:27:22 +0400

    video streaming

--- src/main/java/com/example/demo/controller/ResourceController.java
+++ src/main/java/com/example/demo/controller/ResourceController.java
@@ -2,15 +2,24 @@ package com.example.demo.controller;
 
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,6 +33,8 @@ public class ResourceController {
     public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
 
     private final UserService userService;
+
+    private final ResourceLoader resourceLoader;
 
     @GetMapping(value = "/image/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] getImage(@PathVariable String filename) throws IOException {
@@ -43,5 +54,12 @@ public class ResourceController {
 	Files.write(fileNameAndPath, file.getBytes());
 
 	userService.updatePhoto(id, fileName);
+    }
+
+    @GetMapping(value = "/app/video/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideos(@PathVariable String title) {
+	Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, "11.mp4");
+	return Mono.fromSupplier(() -> new FileSystemResource(fileNameAndPath));
+
     }
 }
